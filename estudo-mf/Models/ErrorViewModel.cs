2cf2351b38df55f96be27cbe9fe224e84bb8b6b1
Danilo Pupin <danilopupin@gmail.com>
2024-04-03 21:30:54 -0300

    Adicionar arquivos de projeto.

---
+++ estudo-mf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace estudo_mf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
