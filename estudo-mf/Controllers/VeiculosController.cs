927cd08d863b3b074dfec05f8d35b8869ed41c46
Danilo Pupin <danilopupin@gmail.com>
2024-04-05 22:37:33 -0300

    Finalizado o CRUD básico

--- estudo-mf/Controllers/VeiculosController.cs
+++ estudo-mf/Controllers/VeiculosController.cs
@@ -35,6 +35,85 @@ namespace estudo_mf.Controllers
 
             return View();
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculos veiculo)
+        {
+            if (id != veiculo.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculo.Update(veiculo);
+
+                _context.Veiculo.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+
+            return View(dados);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Veiculo.Remove(dados);
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+        }
     }
 
 
